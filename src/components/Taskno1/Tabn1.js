57a19bf0337cc5f071e16a2b513ab77c5ac4d7da
Samikhan5823 <84959599+Samikhan5823@users.noreply.github.com>
2022-01-18 14:13:20 +0500

    flower in css

--- src/components/Taskno1/Tabn1.js
+++ src/components/Taskno1/Tabn1.js
@@ -1,13 +1,200 @@
 import React, { Component } from 'react'
 
 export default class Tabn1 extends Component {
-    render() {
-        return (
-            <div>
-                <div class="container">
- 
-</div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="container">
+      <div
+      className="parentFlower"
+        style={{
+          width: "400px",
+          height: "300px",
+          position: "relative",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "absolute",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <div
+            style={{
+              width: "70px",
+              height: "200px",
+              backgroundColor: "red",
+              borderRadius: "48%",
+              border: "3px solid black",
+              position: "absolute",
+              transform: "rotate(-35deg)",
+            }}
+          ></div>
+          <div
+            style={{
+              width: "70px",
+              height: "200px",
+              backgroundColor: "red",
+              borderRadius: "48%",
+              border: "3px solid black",
+              position: "absolute",
+              transform: "rotate(90deg)",
+            }}
+          ></div>
+          <div
+            style={{
+              width: "70px",
+              height: "200px",
+              backgroundColor: "red",
+              borderRadius: "48%",
+              border: "3px solid black",
+              position: "absolute",
+              transform: "rotate(35deg)",
+            }}
+          ></div>
+        </div>
+
+        <div
+          style={{
+            margin: "auto",
+            position: "absolute",
+            width: "100px",
+            height: "100px",
+            borderRadius: "50%",
+            zIndex: 999,
+            backgroundColor: "yellow",
+            overflow: "hidden",
+          }}
+        >
+          {" "}
+          <span
+            style={{
+              width: "100%",
+              border: "1px solid black",
+              display: "block",
+            }}
+          ></span>
+          <span
+            style={{
+              width: "100%",
+              border: "1px solid black",
+              marginTop: "5px",
+              marginBottom: "5px",
+              display: "block",
+            }}
+          ></span>
+          <span
+            style={{
+              width: "100%",
+              border: "1px solid black",
+              display: "block",
+            }}
+          ></span>
+          <span
+            style={{
+              width: "100%",
+              border: "1px solid black",
+              marginTop: "5px",
+              marginBottom: "5px",
+              display: "block",
+            }}
+          ></span>
+          <span
+            style={{
+              width: "100%",
+              border: "1px solid black",
+              display: "block",
+            }}
+          ></span>
+          <span
+            style={{
+              width: "100%",
+              border: "1px solid black",
+              marginTop: "5px",
+              marginBottom: "5px",
+              display: "block",
+            }}
+          ></span>
+          <span
+            style={{
+              width: "100%",
+              border: "1px solid black",
+              display: "block",
+            }}
+          ></span>
+          <span
+            style={{
+              width: "100%",
+              border: "1px solid black",
+              marginTop: "5px",
+              marginBottom: "5px",
+              display: "block",
+            }}
+          ></span>
+          <span
+            style={{
+              width: "100%",
+              border: "1px solid black",
+              display: "block",
+            }}
+          ></span>
+          <span
+            style={{
+              width: "100%",
+              border: "1px solid black",
+              marginTop: "5px",
+              marginBottom: "5px",
+              display: "block",
+            }}
+          ></span>
+          <span
+            style={{
+              width: "100%",
+              border: "1px solid black",
+              display: "block",
+            }}
+          ></span>
+          <span
+            style={{
+              width: "100%",
+              border: "1px solid black",
+              marginTop: "5px",
+              marginBottom: "5px",
+              display: "block",
+            }}
+          ></span>
+          <span
+            style={{
+              width: "100%",
+              border: "1px solid black",
+              display: "block",
+            }}
+          ></span>
+          <span
+            style={{
+              width: "100%",
+              border: "1px solid black",
+              marginTop: "5px",
+              marginBottom: "5px",
+              display: "block",
+            }}
+          ></span>
+          <span
+            style={{
+              width: "100%",
+              border: "1px solid black",
+              display: "block",
+            }}
+          ></span>
+        </div>
+      </div>
+      </div>
+    )
+  }
 }
